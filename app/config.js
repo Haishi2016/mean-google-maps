9d8bdd59dbad0ca6ee3ff4fae3762ce543864212
Demo User <demo@demo-mac.local>
2017-08-20 14:31:32 -0700

    cosmosdb

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ module.exports = {
     mongolab:
     {
         name: "scotch-user-map-ec2",
-        url: "mongodb://scotch:scotchrocks@ds051853.mongolab.com:51853/mean-map-app",
+        url: "mongodb://kgmean:QH1VEHEwFJzojRgggebG2ReDTLFKqRNnpD1JlZKcTIz8knm2oTaAJSsr2UhcsQyyZrT4d4s0ivU1pgFUGTmqfA==@kgmean.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",
         port: 27017
     },
 
